9bc5530f60eb25c4ea30cccab8c9579e121a8d50
Shilpa-axioned <shilpakaxioned.com>
2022-07-12 22:07:38 +0530

    js functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,20 @@ var images = document.querySelectorAll('.gallery>li>a>img'),
  closeIcon = document.querySelector('.menuClose'),
  menuIcon = document.querySelector('.bar'),
  nav = document.querySelector('nav'),
+ title = document.querySelector('.title'),
+ para = document.querySelector('.para'),
  imgSrc;
  menuIcon.classList.remove('bar-active')
 
 images.forEach(function(img) {
-    img.addEventListener("click", function(e) {
+    img.addEventListener("click", function(e) {  
+        var element = img.getAttribute('heading');
+        var desc = img.getAttribute('desc');
         container.classList.add('active');
         imgSrc = e.target.src;
         modalImg.setAttribute("src", imgSrc);
+        title.innerHTML = element;
+        para.innerHTML = desc;
     });
 });
 
@@ -22,8 +28,13 @@ closeBtn.addEventListener('click',function() {
     container.classList.remove('active');
 })
 
-container.addEventListener('click',function() {
+container.addEventListener('click',function(e) {
     container.classList.remove('active');
+    e.stopPropagation();
+})
+
+modal.addEventListener('click',function(e) {
+    e.stopPropagation();
 })
 
 hamburger.addEventListener('click',function() {
